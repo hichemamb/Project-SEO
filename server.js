c6b2c1d41d0b92be26e1e1c9d0e10eb06b8b3466
Hichem AMAR BENSABER <amar-bensaber.h@sfeir.com>
2019-02-25 14:43:08 +0100

    Added static img

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 // Routes
 app.get('/singe-araignees', (req, res) => {
-    res.sendFile(__dirname + '/singe-araignees.html');
+    res.sendFile(__dirname + '/public/singe-araignees.html');
 });
 
 app.get('/mamba-noir', (req, res) => {
-    res.sendFile(__dirname + '/mamba-noir.html');
+    res.sendFile(__dirname + '/public/mamba-noir.html');
 });
 
 app.listen(5000);
